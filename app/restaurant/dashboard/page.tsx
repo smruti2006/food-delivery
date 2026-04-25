fdf562dd8b45e54ee715afc3b2cb93d8dc67de0d
smruti2006 <risingsmruti@gmail.com>
2026-04-25 21:50:47 +0530

    Page wise Authentication

--- app/restaurant/dashboard/page.tsx
+++ app/restaurant/dashboard/page.tsx
@@ -1,9 +1,18 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation';
 import Addnewitem from '../../_components/Addnewitem.jsx'
 import Restaurantheader from '@/app/_components/restaurantheader.jsx';
 const page = () => {
+  const router = useRouter();
   const [additem,setAdditem]= useState(false);
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
+    if(!token){
+      alert("Login first");
+      router.push("/restaurant")
+    }
+  },[])
   return (
     <>
     <Restaurantheader/>
